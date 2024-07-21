a3fab9bb5a5785558bb50a9c0cf0644c9dcc344d
Vladislav Serov <serov.vladsilaw@yandex.ru>
2024-07-21 14:18:32 +0300

    repair test

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: Serov Vlad3islav
+            Author: Serov Vladislav
           </p>
         </div>
       </div>
